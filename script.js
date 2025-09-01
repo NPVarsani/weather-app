6ab114bf10f03cbbe24035e4f7956a8ae783dd3a
Nayan Varsani <70086293+NPVarsani@users.noreply.github.com>
2025-09-01 15:59:35 +0530

    feat: Implement function to fetch weather data from API

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Sensitive Information 
 const apiKey = 'd07349617bf44b32d522cb1642e27435';
+const city = 'London';
 
 async function fetchWeatherData() {
-    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey} `; // Get Url from https://openweathermap.org/api/one-call-3
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`; // url get from https://openweathermap.org/forecast5
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
